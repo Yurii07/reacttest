546c8db43f7ebf4555c73023abd66da29cd12f99
Yurii Adamenko <skyey07@gmail.com>
2019-11-06 22:24:49 +0200

    test

---
+++ precache-manifest.35207da21b83f86c0c37b03f066464ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "201cc0c6070e29344e7ce3aab0e08a5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0eed2579230b1f8608c7",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "daa4ea5dcc1607ef44ed",
+    "url": "/static/js/2.63d17721.chunk.js"
+  },
+  {
+    "revision": "0eed2579230b1f8608c7",
+    "url": "/static/js/main.7f704b4b.chunk.js"
+  },
+  {
+    "revision": "5104823c6be893609e06",
+    "url": "/static/js/runtime-main.fe602e18.js"
+  }
+]);
